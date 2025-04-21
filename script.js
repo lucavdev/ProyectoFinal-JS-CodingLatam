0427eb6666f9625f9d867147d2e582da85a4d9cf
Lucav <valentinomariani11@gmail.com>
2025-04-21 11:44:12 -0300

    2nd commit, finished, las imagenes no renderizaban por la API, no por un error del navegador

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const API_URL = "https://api.escuelajs.co/api/v1";
 const productsContainer = document.querySelector(".products-container");
 
+
 async function getProducts() {
   const response = await fetch(`${API_URL}/products?offset=0&limit=10`);
   const products = await response.json();
@@ -12,30 +13,37 @@ async function getProducts() {
   console.log(products);
 }
 
+
 getProducts();
+
 
 const createProduct = (imageSrc, productName, productPrice) => {
   // Creamos cada elemento
   const elementArticle = document.createElement("article");
   elementArticle.classList.add("product");
 
+
   const elementImage = document.createElement("div");
   elementImage.classList.add("product-image");
 
+
   const elementImg = document.createElement("img");
-  elementImg.src = imageSrc;
+  elementImg.src = imageSrc ;
 
 
   const elementInfo = document.createElement("div");
   elementInfo.classList.add("product-info");
 
+
   const elementName = document.createElement("h3");
   elementName.classList.add("product-name");
   elementName.textContent = productName;
 
+
   const elementPrice = document.createElement("p");
   elementPrice.classList.add("product-price");
   elementPrice.textContent = `$${productPrice}`;
+
 
   // Estructura de los elementos
   productsContainer.append(elementArticle);
